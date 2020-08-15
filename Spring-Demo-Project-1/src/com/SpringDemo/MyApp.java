6ce2c3c51b29923eaf072b9924f6a2e8ad95ead7
Soumen Samanta <soumensamanta39@gmail.com>
2020-08-16 01:50:24 +0530

    Update MyClass.java commited
    with this traditional method we have to create new object everytime , whenever we try to access every class implementation.

--- Spring-Demo-Project-1/src/com/SpringDemo/MyApp.java
+++ Spring-Demo-Project-1/src/com/SpringDemo/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp
 	public static void main(String[] args) 
 	{
 		//create the object of BaseballCoach class
-		Coach ch=new CricketCoach();
+		Coach ch=new CricketCoach(); // with this traditional method we have to create new object everytime , whenever we try to access every class implementation.
 		
 		//use the object
 		System.out.println(ch.getDailyWorkout());
